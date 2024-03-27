dd375ad2454e6b767d2f0be9a06a1e09ca6d2fd0
Hibiki <38676912+Hibiki93@users.noreply.github.com>
2024-03-27 11:50:18 +0800

    Update cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const password = encodeURIComponent("RP1wq8OPiKbRegN2");
 const uri = `mongodb+srv://yue555:${password}@userdb.urornxb.mongodb.net/`;
 const port = 3000;
 
-
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,8 +20,8 @@ const client = new MongoClient(uri, {
   });
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 });
 
 app.get('/',(req,res)=>{
